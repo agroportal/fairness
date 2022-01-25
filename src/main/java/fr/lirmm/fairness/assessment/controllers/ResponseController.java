a0d9c6c1a2ecf7692509077033a67b182686f2f2
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-01-25 17:48:50 +0100

    add portalInstance information in the response status

--- src/main/java/fr/lirmm/fairness/assessment/controllers/ResponseController.java
+++ src/main/java/fr/lirmm/fairness/assessment/controllers/ResponseController.java
@@ -1,8 +1,9 @@
-package fr.lirmm.fairness.assessment;
+package fr.lirmm.fairness.assessment.controllers;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import fr.lirmm.fairness.assessment.models.PortalInstance;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,29 +15,45 @@ public class ResponseController {
 
 
     private final HttpServletResponse servletResponse;
-    private JsonObject response;
+    private final JsonObject response;
     public ResponseController(HttpServletResponse resp) {
         this.servletResponse = resp;
         this.response= new JsonObject();
     }
 
-    private JsonObject getStatus(boolean success, String request, long time, String message) {
+    private JsonObject getStatus(boolean success, String request, long time, String message , RequestController requestController) {
         JsonObject jsonObject = new JsonObject();
         Gson gson = new GsonBuilder().create();
+
         jsonObject.add("request", gson.toJsonTree(request));
         jsonObject.add("success", gson.toJsonTree(success));
         if (message != null && !message.isEmpty()) {
             jsonObject.add("message", gson.toJsonTree(message));
         }
         jsonObject.add("executionTime", gson.toJsonTree(time));
+
+        if(requestController.getPortalInstanceController().getPortalInstanceValue()!=null){
+            PortalInstance portalInstance = requestController.getPortalInstanceController().getPortalInstanceValue();
+            jsonObject.add("endpoint", gson.toJsonTree(portalInstance.getUrl()));
+
+            if(!portalInstance.isPrivateAPI()){
+                jsonObject.add("apikey", gson.toJsonTree(portalInstance.getApikey()));
+            }
+            try {
+                jsonObject.add("useCache", gson.toJsonTree(!requestController.isCacheDisabled()));
+            } catch (Exception ignored) {}
+        }else {
+            jsonObject.add("endpoint", gson.toJsonTree(requestController.getParamController().portalUrl.getValue()));
+        }
+
         return jsonObject;
     }
 
-    public void respond(boolean success , String requestURI , long startTime, String message){
+    public void respond(boolean success , String requestURI , long startTime, String message , RequestController requestController) throws Exception {
 
         PrintWriter out;
 
-        this.response.add("status", getStatus(success, requestURI, System.currentTimeMillis() - startTime, message));
+        this.response.add("status", getStatus(success, requestURI, System.currentTimeMillis() - startTime, message ,requestController));
 
         try {
             out = servletResponse.getWriter();
