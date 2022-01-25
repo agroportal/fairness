297d481d16ba97efdf23309efe7083bf9926434e
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-01-25 17:33:34 +0100

    clean the code from old commented code

--- src/main/java/fr/lirmm/fairness/assessment/utils/converters/AbstractJsonConverter.java
+++ src/main/java/fr/lirmm/fairness/assessment/utils/converters/AbstractJsonConverter.java
@@ -15,35 +15,4 @@ public abstract class AbstractJsonConverter<T> {
 	}
 
 	protected abstract JsonObject toJson();
-/*
-	public JsonObject convertFairToJson() {
-		var jsonObject = new JsonObject();
-		for (AbstractPrinciple p : Fair.getInstance().getPrinciples()) {
-			jsonObject.add(p.toString(), gson.toJsonTree(this.convertToJson(p)));
-		}
-		return jsonObject;
-	}
-
-	private JsonObject convertToJson(AbstractPrinciple p) {
-		var jsonObject = new JsonObject();
-		for (AbstractPrincipleCriterion c : p.getPrincipleCriteria()) {
-			jsonObject.add(c.toString(), gson.toJsonTree(this.convertToJson(c)));
-		}
-		jsonObject.add("totalScore", gson.toJsonTree(p.getTotalScore()));
-		return jsonObject;
-	}
-
-	private JsonObject convertToJson(AbstractPrincipleCriterion c) {
-		var jsonObject = new JsonObject();
-		jsonObject.add("resultSet", gson.toJsonTree(this.convertToJson(c.getResultSet())));
-		return jsonObject;
-	}
-
-	private JsonObject convertToJson(ResultSet rs) {
-		var jsonObject = new JsonObject();
-		jsonObject.add("explanations", gson.toJsonTree(rs.getExplanations()).getAsJsonArray());
-		jsonObject.add("scores", gson.toJsonTree(rs.getScores()).getAsJsonArray());
-		jsonObject.add("totalScore", gson.toJsonTree(rs.getTotalScore()));
-		return jsonObject;
-	}*/
 }
