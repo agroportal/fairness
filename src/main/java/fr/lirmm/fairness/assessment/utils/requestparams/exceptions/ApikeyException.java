17a1b57507df97fea5bd776687403450c5e8c87d
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-01-25 17:25:37 +0100

    add apikey error exception

---
+++ src/main/java/fr/lirmm/fairness/assessment/utils/requestparams/exceptions/ApikeyException.java
@@ -0,0 +1,7 @@
+package fr.lirmm.fairness.assessment.utils.requestparams.exceptions;
+
+public class ApikeyException extends  Exception{
+    public ApikeyException(String message) {
+        super(message);
+    }
+}
